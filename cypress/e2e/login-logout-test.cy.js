ddd88245e13218b9b0fe860e7d7b7911dbf9781f
Regine Kornbakk <142594277+TheRegzi@users.noreply.github.com>
2024-10-15 13:12:10 +0200

    Added logout test to the cypress test

--- cypress/e2e/login-logout-test.cy.js
+++ cypress/e2e/login-logout-test.cy.js
@@ -1,4 +1,4 @@
-describe('Login test', () => {
+describe('Login and logout test', () => {
   beforeEach(() => {
     cy.visit('https://norofffeu.github.io/social-media-client/');
 
@@ -61,5 +61,41 @@ describe('Login test', () => {
     });
 
     cy.get('#loginForm').submit();
+  });
+
+  it('should allow a user to log in with valid credentials', () => {
+    cy.get('#registerModal').should('be.visible');
+
+    cy.get(
+      '#registerModal .modal-footer button.btn-outline-success[data-bs-target="#loginModal"]',
+    )
+      .should('be.visible')
+      .click({ force: true, x: 5, y: 5 });
+
+    cy.window().then((win) => {
+      win.document.querySelector('#registerModal').style.display = 'none';
+    });
+
+    cy.get('#loginModal').should('be.visible', { force: true });
+
+    cy.get('#loginEmail').type('testuser@stud.noroff.no');
+    cy.get('#loginPassword').type('testing123');
+
+    cy.get('#loginForm').submit();
+
+    cy.contains('Logout').should('be.visible');
+
+    cy.window().then((win) => {
+      const registerModal = win.document.querySelector('#registerModal');
+      if (registerModal && registerModal.style.display !== 'none') {
+        registerModal.style.display = 'none';
+      }
+    });
+
+    cy.get('button[data-auth="logout"]')
+      .should('be.visible')
+      .click({ force: true });
+
+    cy.get('#registerModal').should('be.visible');
   });
 });
